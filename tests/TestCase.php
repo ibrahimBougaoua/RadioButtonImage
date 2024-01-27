73daf6d225f68402f1d119951e672cd77e6733e7
ibrahimBougaoua <ibrahimBougaoua@users.noreply.github.com>
2024-01-27 18:23:29 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,9 @@
 
 namespace IbrahimBougaoua\RadioButtonImage\Tests;
 
+use IbrahimBougaoua\RadioButtonImage\RadioButtonImageServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use IbrahimBougaoua\RadioButtonImage\RadioButtonImageServiceProvider;
 
 class TestCase extends Orchestra
 {
